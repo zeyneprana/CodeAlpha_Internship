83d6936a9c06400fbaade35f65705395218401b8
Zeynep Rana Uyar <zranawawa@gmail.com>
2025-11-12 17:56:05 +0300

    Image Gallery - Task 1 / Fourth Commit
    BaguetteBox (lightbox) and hover effect (in style.css file) added.

--- Image Gallery - Task 1/js/index.js
+++ Image Gallery - Task 1/js/index.js
@@ -8,3 +8,7 @@ $('#nav-filter').on( 'click', 'button', function() {
   var filterValue = $(this).attr('data-filter');
   $('#nav-content').isotope({ filter: filterValue });
 });
+
+baguetteBox.run('#nav-content', {
+  captions: true
+});
